682d05dc525be47e650fd0689bd5382de2b309b8
arifiani1009 <arifiani.tc09@gmail.com>
2022-09-16 20:42:32 +0700

    Update main.js

--- 2. Menggambar 3 Titik/main.js
+++ 2. Menggambar 3 Titik/main.js
@@ -1,1 +1,49 @@
+function main(){
+    var canvas = document.getElementById("myCanvas");
+    var gl = canvas.getContext("webgl");
 
+    var vertices = [
+        0.2, 0.3,   //titik A
+        0.2, 0.0,   //titik B
+        -0.3, 0.0   //titik C
+    ];
+
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var vertexShaderCode = document.getElementById("vertexShaderCode").text;
+    //buat vertex shader
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
+
+    //buat fragment
+    var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    //membuat program agar bisa dieksekusi
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
+    //mengatur background
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //menggambar titik
+    var primitive = gl.POINTS;
+    var count = 3;
+    var offset = 0;
+    gl.drawArrays(primitive, offset, count);
+}
